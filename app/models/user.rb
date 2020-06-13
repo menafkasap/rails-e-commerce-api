f63380964f21251c3db5bcade983f21e06dac359
Abdulmenaf Kasap <abdulmenaf.kasap@gmail.com>
2020-06-13 18:55:48 +0300

    Create user model and controller

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+end
